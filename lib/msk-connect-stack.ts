39ccc90811c9abcc73423df775a41911944361b4
Nick Franken <shnick@gmail.com>
2023-04-08 01:43:13 +0000

    init

---
+++ lib/msk-connect-stack.ts
@@ -0,0 +1,32 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnCluster } from 'aws-cdk-lib/aws-msk'
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+
+export class MskConnectStack extends Stack {
+  props: StackProps;
+
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+    this.props = props;
+    this.build();
+  }
+
+  build () {
+    const vpc = new Vpc(this, "msk-vpc", {
+      maxAzs: 2,
+    })
+    vpc.privateSubnets[0].subnetId
+
+    const mskCluster = new CfnCluster(this, "msk-cluster", {
+      clusterName: 'msk-cluster',
+      kafkaVersion: '2.8.0',
+      numberOfBrokerNodes: 3,
+      brokerNodeGroupInfo: {
+        instanceType: "kafka.t3.small",
+        clientSubnets: vpc.privateSubnets.map(s => s.subnetId)
+      }
+    })
+  }
+}
---
+++ test/msk-connect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MskConnect from '../lib/msk-connect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/msk-connect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MskConnect.MskConnectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
